0f5641c427e9928fc2da56d6ded91abc77caee33
Yassine <yassine.ezzedini@esprit.tn>
2020-12-12 20:47:30 +0100

    added navigation menu and now i'm able to nagivate my app and added home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
   
 import { PostModule } from './post/post.module';
+import { HomeComponent } from './home/home.component';
   
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -4,8 +4,11 @@ import { IndexComponent } from './index/index.component';
 import { ViewComponent } from './view/view.component';
 import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
+import { HomeComponent } from '../home/home.component';
   
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch:'full'},
+  { path: 'home', component: HomeComponent},
   { path: 'post', redirectTo: 'post/index', pathMatch: 'full'},
   { path: 'post/index', component: IndexComponent },
   { path: 'post/:postId/view', component: ViewComponent },
